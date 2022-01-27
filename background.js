569b9e2a8a42a4ced9bbe4ff64125454bb6fd220
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-01-27 09:56:59 +0300

    add context menus for the extension

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
 let color = "#3aa757";
+const contextMenusItems = ["disable cookie popup", "disable sign up popup"];
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color });
   console.log("default color is " + color);
+  for (const menu of contextMenusItems) {
+    chrome.contextMenus.create({
+      id: menu,
+      title: menu,
+      type: "normal",
+      contexts: ["selection"],
+    });
+  }
 });
