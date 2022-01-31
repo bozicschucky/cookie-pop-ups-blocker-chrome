51048047265306025c74ee823f6cdd9d46b8397b
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-01-31 10:04:50 +0300

    remove used code

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
-let color = "#3aa757";
 const contextMenusItems = ["disable cookie popup", "disable sign up popup"];
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color });
-  console.log("default color is " + color);
-  for (const menu of contextMenusItems) {
-    chrome.contextMenus.create({
-      id: menu,
-      title: menu,
-      type: "normal",
-      contexts: ["selection"],
-    });
-  }
+chrome.runtime.onInstalled.addListener(async function () {
+  console.log("first time install");
+  let tab = await getCurrentTab();
+  console.log("🚀 ~ file: background.js ~ line 12 ~ tab", tab);
+});
+
+//listen for messages from contentScript.js
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  console.log("messages -->", msg);
 });
