da92762e61f7d8917270d36d15267e5bbf20e43d
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-01-31 10:04:40 +0300

    try to get the dom  when a btn is clicked

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("start here");
+const htmlDiv = document.getElementById("html");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  console.log("the btn has been clicked");
+
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    var tab = tabs[0];
+    console.log(tab.url, tab.title);
+
+    chrome.tabs.sendMessage(tab.id, { greeting: "getDom" }, function (msg) {
+      msg = msg || {};
+      console.log("onResponse", msg.msg);
+    });
+  });
+});
