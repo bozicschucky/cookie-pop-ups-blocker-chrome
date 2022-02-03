4f5d98b8a28d8bfc7dec9aa257bcdfa1cf146d60
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-02-03 09:09:59 +0300

    add logic to remove cookie pop ups

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,48 @@
+const removeCookieElements = () => {
+  const dom = document.querySelectorAll("*");
+  // regex to match a sentence that contains the word cookie(s) case insensitive
+  const regex = /cookie(s)?/gi;
+  // regex that that matches a sentence that contains this site or this website
+  const thisSiteRegex = /(this site|this website)/gi;
+  // regex that matches a sentence that contains the word uses cookie(s) case insensitive
+  const usesCookieRegex = /uses cookie(s)?/gi;
+
+  // find all the elements that have the word cookie(s) in their text
+  const AllCookieElements = Array.from(dom).filter(
+    (el) =>
+      (el.textContent.match(regex) && el.textContent.match(thisSiteRegex)) ||
+      el.textContent.match(usesCookieRegex)
+  );
+  let cookieElms = [];
+
+  // filter out the elements that are body or html
+  AllCookieElements.forEach((el) => {
+    if (el.tagName !== "BODY" && el.tagName !== "HTML") {
+      cookieElms.push(el);
+    }
+  });
+
+  // remove the cookie divs
+  cookieElms.forEach((el) => el.remove());
+
+  // make the cookie divs invisible
+  // cookieElms.forEach((el) => {
+  //   el.style.display = "none";
+  // });
+};
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  const dom = document.documentElement.outerHTML;
-  console.log("onMessage", msg, dom);
   if (msg.greeting == "getDom") {
     sendResponse({
       msg: "dom loaded",
       dom,
     });
-  } else {
-    sendResponse({});
+  }
+  if (msg.msg === "cookieBlockerChecked") {
+    console.log(msg.msg);
+    removeCookieElements();
+  }
+  if (msg.msg === "cookieBlockerUnChecked") {
+    console.log(msg.msg);
   }
 });
--- popup.js
+++ popup.js
@@ -1,16 +1,28 @@
 const htmlDiv = document.getElementById("html");
 const btn = document.getElementById("btn");
+const cookieCheckBox = document.getElementById("cookie-pop-up-check-input");
+const signUpCheckBox = document.getElementById("sign-up-pop-up-check-input");
+
+const getCurrentTab = async () => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+};
+
+const sendMessageToContentScript = async (msg) => {
+  let tab = await getCurrentTab();
+  chrome.tabs.sendMessage(tab.id, msg);
+};
 
 btn.addEventListener("click", (e) => {
-  console.log("the btn has been clicked");
+  sendMessageToContentScript({ greeting: "getDom" });
+});
 
-  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-    var tab = tabs[0];
-    console.log(tab.url, tab.title);
-
-    chrome.tabs.sendMessage(tab.id, { greeting: "getDom" }, function (msg) {
-      msg = msg || {};
-      console.log("onResponse", msg.msg);
-    });
-  });
+cookieCheckBox.addEventListener("click", (e) => {
+  if (e.target.checked) {
+    sendMessageToContentScript({ msg: "cookieBlockerChecked" });
+  } else {
+    console.log("checked not checked");
+    sendMessageToContentScript({ msg: "cookieBlockerUnChecked" });
+  }
 });
